202aa1d630174629c5a0567b6034ba29f416eca0
mamun-id <abdullahalmamun@ipvisionsoft.com>
2019-09-23 20:29:10 +0600

    Xcode Project created

---
+++ Basic Proactice/Basic Proactice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Basic Proactice
+//
+//  Created by Mamun on 9/23/19.
+//  Copyright © 2019 Mamun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
